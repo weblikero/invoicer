6fd6460a55f7d5bb7984c2508338eb5c7ef3a433
PovilasKorop <povilask007@gmail.com>
2020-02-21 19:20:27 +0200

    Fixed "Add row" button
    Otherwise there's an error "e not defined"

--- resources/views/invoices/create.blade.php
+++ resources/views/invoices/create.blade.php
@@ -144,7 +144,7 @@
     <script>
         $(document).ready(function(){
             var i=1;
-            $("#add_row").click(function(){b=i-1;
+            $("#add_row").click(function(e){b=i-1;
                 $('#addr'+i).html($('#addr'+b).html()).find('td:first-child').html(i+1);
                 $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
                 i++;
